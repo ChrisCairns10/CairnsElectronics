c8298d882533cc6eb06cbf121197d5754f1eab45
ChrisCairns10 <103499469+ChrisCairns10@users.noreply.github.com>
2023-05-22 14:57:10 +1200

    Delete BIT703_A2_5049292/AS2 directory

--- BIT703_A2_5049292/AS2/5049292/js/clearLocalStorage.js
+++
@@ -1,4 +0,0 @@
-/* Testing Purposes: 
-Clear Local Storage quantity when opening Home page or returning to Home page */
-
-localStorage.setItem('Quantity', 0);
--- BIT703_A2_5049292/AS2/5049292/js/updateShoppingCartQuantity.js
+++
@@ -1,4 +0,0 @@
-/* Retrieves chosen product quantity from Local Storage and updates Shopping Cart */
-
-var cartQuan = localStorage.getItem("Quantity");
-document.getElementById("cartQuantity").value = cartQuan;
